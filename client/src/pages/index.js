0adaf21cc7870c90835bdf056e79437bfd8c5968
ejionaut <epangwi@gmail.com>
2022-11-13 16:54:44 +0800

    Error handling and small styling

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,24 +1,50 @@
-import React from "react"
-import "./styles/index.css"
-import logo from "./styles/images/logo_square.png"
+import React from "react";
+import "./styles/index.css";
+import logo from "./styles/images/logo_square.png";
+import { useState } from "react";
 
-const loginModule = () => {
+const LoginModule = () => {
+    const [userId, setUserId] = useState("");
+    const [passwordId, setPassword] = useState("");
+
     return(
         <main className="Main_Section">
+            <div id="error_message" className="error_message">
+                <h1 id="error_message_text">e</h1>
+            </div>
             <img src={logo} alt="" />
             <div className="input_section">
                 <div className="username_section">
                     <label className="username_label"> User ID: </label>
-                    <input type="text"/>
+                    <input type="text" onChange={(event) => {
+                        setUserId(event.target.value);
+                    }}/>
                 </div>
                 <div className="password_section">
                     <label className="password_label"> Password: </label>
-                    <input type="password" />
+                    <input type="password" onChange={(event) => {
+                        setPassword(event.target.value);
+                    }}/>
                 </div>
-                <button> Login </button>
+                <button onClick={errorChoice(userId, passwordId)}> Login </button>
             </div>
         </main>
     );
 };
 
-export default loginModule;
+function errorChoice(userId, passwordId) {
+    const codeID = "test";
+    const passID = "test";
+
+    if (userId.value === '' || userId.value == null && passwordId.value === '' || passwordId.value === null) {
+        document.getElementById('error_message_text').innerHTML = "<span>ERROR:</span> The input is blank";
+        document.getElementById('error_message').style.visibility = "visible";
+    } else if (userId.value !== codeID.value && passID.value !== passwordId.value) {
+        document.getElementById('error_message_text').innerHTML = "<span>ERROR:</span> The username and password does not match";
+        document.getElementById('error_message').style.visibility = "visible";
+    } else if (userId.value === codeID.value && passID === passwordId.value) {
+
+    }
+}
+
+export default LoginModule;
